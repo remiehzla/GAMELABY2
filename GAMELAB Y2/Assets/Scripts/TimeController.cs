19fc2d19cbb348781cf93cf5fdf114bf1306217a
Sonia Cailean <remiehzla02@gmail.com>
2023-04-29 17:51:34 +0300

    Cycle based on time

--- GAMELAB Y2/Assets/Scripts/TimeController.cs
+++ GAMELAB Y2/Assets/Scripts/TimeController.cs
@@ -15,18 +15,35 @@ public class TimeController : MonoBehaviour
     [SerializeField]
     private Text timeText;
 
+    [SerializeField]
+    private Light sunLight;
+
+    [SerializeField]
+    private float sunriseHour;
+
+    [SerializeField]
+    private float sunsetHour;
+
     private DateTime currentTime;
+
+    private TimeSpan sunriseTime;
+
+    private TimeSpan sunsetTime;
 
     // Start is called before the first frame update
     void Start()
     {
         currentTime = DateTime.Now.Date + TimeSpan.FromHours(startHour);
+
+        sunriseTime = TimeSpan.FromHours(sunriseHour);
+        sunsetTime = TimeSpan.FromHours(sunsetHour);
     }
 
     // Update is called once per frame
     void Update()
     {
-        UpdateTimeOfDay(); 
+        UpdateTimeOfDay();
+        RotateSun();
     }
 
     //Add to the current time
@@ -37,5 +54,45 @@ public class TimeController : MonoBehaviour
         {
             timeText.text = currentTime.ToString("HH:mm");
         }
+    }
+
+    private void RotateSun()
+    {
+        float sunLightRotation;
+
+        if (currentTime.TimeOfDay > sunriseTime && currentTime.TimeOfDay < sunsetTime)
+        {
+            TimeSpan sunriseToSunsetDuration = CalculateTimeSpanDifference(sunriseTime, sunsetTime);
+            TimeSpan timeSinceSunrise = CalculateTimeSpanDifference(sunriseTime, currentTime.TimeOfDay);
+
+            double percentage = timeSinceSunrise.TotalMinutes / sunriseToSunsetDuration.TotalMinutes;
+
+            sunLightRotation = Mathf.Lerp(0, 100, (float)percentage);
+
+        }
+        else
+        {
+            TimeSpan sunsetToSunriseDuration = CalculateTimeSpanDifference(sunsetTime, sunriseTime);
+            TimeSpan timeSinceSunset = CalculateTimeSpanDifference(sunsetTime, currentTime.TimeOfDay);
+
+            double percentage = timeSinceSunset.TotalMinutes / sunsetToSunriseDuration.TotalMinutes;
+
+            sunLightRotation = Mathf.Lerp(100, 360, (float)percentage);
+        }
+
+        sunLight.transform.rotation = Quaternion.AngleAxis(sunLightRotation, Vector3.right);
+    }
+    
+    // Time difference formula
+    private TimeSpan CalculateTimeSpanDifference(TimeSpan fromTime, TimeSpan toTime)
+    {
+        TimeSpan difference = toTime - fromTime;
+
+        if (difference.TotalSeconds < 0)
+        {
+            difference += TimeSpan.FromHours(24);
+        }
+
+        return difference;
     }
 }
