f0f4d2eb7aee82942b4def476c3d6311c21c8bf9
Sonia Cailean <remiehzla02@gmail.com>
2023-04-30 16:00:08 +0300

    Day night cycle 1.0 done

--- GAMELAB Y2/Assets/Scripts/TimeController.cs
+++ GAMELAB Y2/Assets/Scripts/TimeController.cs
@@ -110,8 +110,10 @@ public class TimeController : MonoBehaviour
         sunLight.intensity = Mathf.Lerp(0, maxSunLightIntensity, lightChangeCurve.Evaluate(dotProduct));
         moonLight.intensity = Mathf.Lerp(maxMoonLightIntensity, 0, lightChangeCurve.Evaluate(dotProduct));
 
-        RenderSettings.ambientLight = Color.Lerp(nightAmbientLight, dayAmbientLight, 
-            lightChangeCurve.Evaluate(dotProduct));
+        RenderSettings.ambientLight = Color.Lerp(nightAmbientLight, dayAmbientLight, dotProduct);
+
+        Debug.Log($"Dot Product = {dotProduct}, Light Change Curve Value = {lightChangeCurve.Evaluate(dotProduct)}, " +
+            $"Sun Intensity = {sunLight.intensity}, Moon Intensity = {moonLight.intensity}");
     }
 
     // Time difference formula
