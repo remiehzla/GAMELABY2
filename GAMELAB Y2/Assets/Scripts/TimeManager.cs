55343288514ebdd8d62dd7a37fa5d97edc2ab60a
Sonia Cailean <remiehzla02@gmail.com>
2023-04-30 18:06:31 +0300

    Day night cycle per rounds done

--- GAMELAB Y2/Assets/Scripts/TimeManager.cs
+++ GAMELAB Y2/Assets/Scripts/TimeManager.cs
@@ -5,14 +5,48 @@ using UnityEngine;
 public class TimeManager : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private GameManager gameManager;
+
+    [SerializeField] private float targetRot;
+    [SerializeField] private float rotSpeed;
+
+    //private bool _isDay = true;
+
+    [SerializeField] private Light sunLight;
+
     void Start()
     {
-        
+        gameManager = FindObjectOfType<GameManager>();
+        targetRot = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (gameManager.round <= gameManager.maxRounds)
+        {
+            if (gameManager.turn == 0)
+            {
+                targetRot = 20f;
+            }
+            else if (gameManager.turn >= 1 && gameManager.turn <= 4)
+            {
+                targetRot = -60;
+            }
+            else if (gameManager.turn > 4)
+            {
+                targetRot = 0f;
+            }
+
+            Quaternion targetQuaternion = Quaternion.Euler(targetRot, 0f, 0f);
+            sunLight.transform.rotation = Quaternion.Lerp(sunLight.transform.rotation, 
+                targetQuaternion, rotSpeed * Time.deltaTime);
+        }
+    }
+
+    public void DayNightCycle()
+    { 
         
     }
 }
