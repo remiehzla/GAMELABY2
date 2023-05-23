c3bb1e5851f07725e9d36f68e9bc3bfccb7e9ca3
Sonia Cailean <64458929+remiehzla@users.noreply.github.com>
2023-05-23 23:49:58 +0200

    (Unstable) Earthquake Event
    Need to find the most optimal way to limit the event to happen only once per turn

--- GAMELAB Y2/Assets/Scripts/RandomEvents.cs
+++ GAMELAB Y2/Assets/Scripts/RandomEvents.cs
@@ -5,29 +5,47 @@ using UnityEngine;
 public class RandomEvents : MonoBehaviour
 {
     private GameManager gameManager;
-    private Tile existingPrompt;
-    
+   // private Tile existingPrompt;
 
-    // One event is always neutral
+    // The four player' maps
+    public GameObject map1;
+    public GameObject map2;
+    public GameObject map3;
+    public GameObject map4;
+
+    public int numberOfTiles = 22;
+    public int randomTile;
+
+    [SerializeField] private bool eventTrigger;
+    [SerializeField] private int randomNumber;
+
+    // One event must be neutral
     private int numberOfEvents = 6;
+
+    private void Start()
+    {
+        //randomTile = null;
+        gameManager = FindObjectOfType<GameManager>();
+        eventTrigger = false;
+        ChooseEvent();
+    }
 
     void Update()
     {
         if (gameManager.round <= gameManager.maxRounds)
         {
             if (gameManager.turn != 0)
-            {
-                int randomNumber = Random.Range(0, numberOfEvents);
-
+            {  
                 switch (randomNumber)
                 {
                     case 0:
-                        EarthquakeEvent();
+                        Invoke("EarthquakeEvent", 1);
+                        eventTrigger = true;
                         break;
                     case 1:
-                        BadConstructionEvent();
+                        NoEvent();
                         break;
-                    case 2:
+                    /*case 2:
                         FireEvent();
                         break;
                     case 3:
@@ -37,21 +55,81 @@ public class RandomEvents : MonoBehaviour
                         CounterClockTurnsEvent();
                         break;
                     case 5:
-                        NoEvent();
-                        break; 
+                        BadConstructionEvent();
+                        break;*/
 
                 }
+
+               // EarthquakeEvent();
             }
         }
     }
 
+    private void ChooseEvent()
+    {
+        randomNumber = Random.Range(0, 2);
+    }
+
     private void EarthquakeEvent()
-    {   
+    {
         // Choose a random tile, may it have a prompt built or not
         // Play a shaking + move down animation
         // Destory tile
+
+        switch (gameManager.turn)
+        {
+            case 1:
+                {
+                    // P1
+                    if (eventTrigger)
+                    { 
+                        eventTrigger = false;
+                        randomTile = Random.Range(0, numberOfTiles);
+                        Invoke("ChooseEvent", 1);
+                    }
+                    map1.transform.GetChild(randomTile).gameObject.SetActive(false);
+                    
+                    break;
+                }
+            case 2:
+                {
+                    // P2
+                    if (eventTrigger)
+                    {
+                        eventTrigger = false;
+                        randomTile = Random.Range(0, numberOfTiles);
+                        Invoke("ChooseEvent", 1);
+                    }
+                    map2.transform.GetChild(randomTile).gameObject.SetActive(false);
+                    break;
+                }
+            case 3:
+                {
+                    // P3
+                    if (eventTrigger)
+                    {
+                        eventTrigger = false;
+                        randomTile = Random.Range(0, numberOfTiles);
+                        Invoke("ChooseEvent", 1);
+                    }
+                    map3.transform.GetChild(randomTile).gameObject.SetActive(false);
+                    break;
+                }
+            case 4:
+                {
+                    // P4
+                    if (eventTrigger)
+                    {
+                        eventTrigger = false;
+                        randomTile = Random.Range(0, numberOfTiles);
+                        Invoke("ChooseEvent", 1);
+                    }
+                    map4.transform.GetChild(randomTile).gameObject.SetActive(false);
+                    break;
+                }
+        }
     }
-    
+
     private void BadConstructionEvent()
     { 
         // Choose a random prompt
