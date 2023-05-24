3cfa1736d4673312edceaeeeace4c5e8832bb4b8
Sonia Cailean <remiehzla02@gmail.com>
2023-05-24 09:44:32 +0200

    (Stable) Earthquake event
    Must check the RandomVariable; seems static

--- GAMELAB Y2/Assets/Scripts/RandomEvents.cs
+++ GAMELAB Y2/Assets/Scripts/RandomEvents.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class RandomEvents : MonoBehaviour
 {
     private GameManager gameManager;
-   // private Tile existingPrompt;
 
     // The four player' maps
     public GameObject map1;
@@ -13,61 +12,48 @@ public class RandomEvents : MonoBehaviour
     public GameObject map3;
     public GameObject map4;
 
+    // The map's tiles
     public int numberOfTiles = 22;
     public int randomTile;
 
-    [SerializeField] private bool eventTrigger;
+    // Choosing events
+    [SerializeField] private int currentTurn;
     [SerializeField] private int randomNumber;
-
-    // One event must be neutral
-    private int numberOfEvents = 6;
 
     private void Start()
     {
-        //randomTile = null;
         gameManager = FindObjectOfType<GameManager>();
-        eventTrigger = false;
-        ChooseEvent();
     }
 
     void Update()
     {
-        if (gameManager.round <= gameManager.maxRounds)
-        {
-            if (gameManager.turn != 0)
-            {  
-                switch (randomNumber)
-                {
-                    case 0:
-                        Invoke("EarthquakeEvent", 1);
-                        eventTrigger = true;
-                        break;
-                    case 1:
-                        NoEvent();
-                        break;
-                    /*case 2:
-                        FireEvent();
-                        break;
-                    case 3:
-                        StormEvent();
-                        break;
-                    case 4:
-                        CounterClockTurnsEvent();
-                        break;
-                    case 5:
-                        BadConstructionEvent();
-                        break;*/
-
-                }
-
-               // EarthquakeEvent();
-            }
-        }
+        if (currentTurn != gameManager.turn)
+            //Invoke("ChooseEvent", 1);
+            ChooseEvent();
+        currentTurn = gameManager.turn;
     }
 
     private void ChooseEvent()
     {
-        randomNumber = Random.Range(0, 2);
+        int randomNumber = Random.Range(0, 2);
+
+        if (gameManager.round <= gameManager.maxRounds)
+        {
+            if (gameManager.turn != 0)
+            {
+                // The events
+                switch (randomNumber)
+                {
+                    case 0:
+                        NoEvent();
+                        break;
+
+                    case 1:
+                        Invoke("EarthquakeEvent", 1);
+                        break;
+                }
+            }
+        }
     }
 
     private void EarthquakeEvent()
@@ -81,80 +67,27 @@ public class RandomEvents : MonoBehaviour
             case 1:
                 {
                     // P1
-                    if (eventTrigger)
-                    { 
-                        eventTrigger = false;
-                        randomTile = Random.Range(0, numberOfTiles);
-                        Invoke("ChooseEvent", 1);
-                    }
+                    randomTile = Random.Range(0, numberOfTiles);
                     map1.transform.GetChild(randomTile).gameObject.SetActive(false);
-                    
+
                     break;
                 }
             case 2:
                 {
                     // P2
-                    if (eventTrigger)
-                    {
-                        eventTrigger = false;
-                        randomTile = Random.Range(0, numberOfTiles);
-                        Invoke("ChooseEvent", 1);
-                    }
-                    map2.transform.GetChild(randomTile).gameObject.SetActive(false);
                     break;
                 }
             case 3:
                 {
                     // P3
-                    if (eventTrigger)
-                    {
-                        eventTrigger = false;
-                        randomTile = Random.Range(0, numberOfTiles);
-                        Invoke("ChooseEvent", 1);
-                    }
-                    map3.transform.GetChild(randomTile).gameObject.SetActive(false);
                     break;
                 }
             case 4:
                 {
                     // P4
-                    if (eventTrigger)
-                    {
-                        eventTrigger = false;
-                        randomTile = Random.Range(0, numberOfTiles);
-                        Invoke("ChooseEvent", 1);
-                    }
-                    map4.transform.GetChild(randomTile).gameObject.SetActive(false);
                     break;
                 }
         }
-    }
-
-    private void BadConstructionEvent()
-    { 
-        // Choose a random prompt
-        // Play smoke particle effect
-        // Destroy it
-    }
-
-    private void FireEvent()
-    { 
-        // Choose a random prompt
-        // Play fire particle effect
-        // Destroy it
-    }
-
-    private void StormEvent()
-    { 
-        // Choose a random prompt
-        // Play wind patricle effect
-        // Destroy it
-    }
-
-    private void CounterClockTurnsEvent()
-    { 
-        // Counter clock turns
-        // Game goes on
     }
 
     private void NoEvent()
